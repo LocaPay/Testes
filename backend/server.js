8aa769e297f5394d237381346c4083cd8c512350
Marcos <terceiro.marcos2005@gmail.com>
2025-07-24 15:31:00 -0300

    adicionando permissao de cors para o frontend de testes

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://locapay-copy-production.up.railway.app",
+      "https://locapay-copy-production.up.railway.app/",
     ],
     methods: ["GET", "POST", "PUT"],
     credentials: true,
