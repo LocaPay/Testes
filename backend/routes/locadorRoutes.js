0933bfc227e4b80f6953e1c726fe72e1ad41f739
Marcos <terceiro.marcos2005@gmail.com>
2025-07-25 16:57:12 -0300

    ajustando metodo http

--- backend/routes/locadorRoutes.js
+++ backend/routes/locadorRoutes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.post('/locadores', locadorCtrl.registrar);
 router.post('/login', locadorCtrl.login);
 router.get('/info', auth, locadorCtrl.buscarDadosGerais);
-router.get('/buscar-cus-id', locadorCtrl.buscarIdPorCustomerId);
+router.post('/buscar-id-por-cus', locadorCtrl.buscarIdPorCustomerId);
 router.get('/buscar-info-subconta/:locadorId', locadorCtrl.buscarinfosubConta);
 router.get('/locador/por-inquilino/:inquilino_id', locadorCtrl.buscarLocadorPorInquilino );
 router.get('/buscar-saldo', auth, locadorCtrl.buscarSaldoLocador);
