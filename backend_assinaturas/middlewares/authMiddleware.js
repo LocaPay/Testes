d94263252eb27fdfd8bb5cae00a508afa1bdbcf8
Marcos <terceiro.marcos2005@gmail.com>
2025-07-25 16:40:18 -0300

    adicionando reconhecimento de autenticação do asaas

--- backend_assinaturas/middlewares/authMiddleware.js
+++ backend_assinaturas/middlewares/authMiddleware.js
@@ -1,8 +1,11 @@
 module.exports = (req, res, next) => {
   const authHeader = req.headers.authorization;
+  const authHeaderAsaas = req.headers.asaas - access - token;
   const apiKey = process.env.ASSINATURAS_API_KEY;
 
   if (!authHeader || authHeader !== `Bearer ${apiKey}`) {
+    return res.status(403).json({ erro: "Acesso não autorizado" });
+  } else if (!authHeaderAsaas || authHeaderAsaas !== `Bearer ${apiKey}`) {
     return res.status(403).json({ erro: "Acesso não autorizado" });
   }
 
